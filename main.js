0590a51c4b67c3b6bab98f34fdcbaf42ea9c2d33
sh.james <378736618@qq.com>
2018-10-18 13:20:04 +0800

    测试模块和 异步回调

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 //非异步
 // var fs = require("fs");
-
 // var data = fs.readFileSync('input.txt');
-
 // console.log(data.toString());
 // console.log("程序执行结束!");
 
 //下面是回调，可以异步执行
-var fs = require("fs");
+// var fs = require("fs");
+// fs.readFile('test_fs.txt', function (err, data) {
+//     if (err) return console.error(err);
+//     console.log(data.toString());
+// });
 
-fs.readFile('input.txt', function (err, data) {
-    if (err) return console.error(err);
-    console.log(data.toString());
-});
+// console.log("程序执行结束!");
 
-console.log("程序执行结束!");
+// 异步回调
+// function aa(func) {
+//     setTimeout(() => {
+//         func('msg');
+//     }, 2000);
+// }
+
+// aa((msg) => {
+//     console.log('func01-' + msg)
+// });
+
+// console.log('func00')
+
+// 自定义模块
+var module_test = require('./module_test')
+// console.log(module_test)
+module_test('james')
